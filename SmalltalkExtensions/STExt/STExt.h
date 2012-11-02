032ee6f58366c6ca5937c2c9e3be4bc230e8f3b4
Oscar E A Callaú <oscar.apollo@gmail.com>
2012-11-02 17:50:45 -0300

    Added a lib build + headers

---
+++ SmalltalkExtensions/STExt/STExt.h
@@ -0,0 +1,17 @@
+//
+//  STExt.h
+//  STExt
+//
+//  Created by Oscar E A Callaú on 10/18/12.
+//  Copyright (c) 2012 Uchile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "NSMutableArray+SmalltalkCollections.h"
+#import "NSArray+SmalltalkCollections.h"
+#import "NSString+SmalltalkString.h"
+
+@interface STExt : NSObject
+
+@end
---
+++ SmalltalkExtensions/STExt/STExt.m
@@ -0,0 +1,13 @@
+//
+//  STExt.m
+//  STExt
+//
+//  Created by Oscar E A Callaú on 10/18/12.
+//  Copyright (c) 2012 Uchile. All rights reserved.
+//
+
+#import "STExt.h"
+
+@implementation STExt
+
+@end
