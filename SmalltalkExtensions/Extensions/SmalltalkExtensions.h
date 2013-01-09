ba1bca4c1de0e1d2f61e7a00d83c58646abac5e1
bracken-dev <bracken.spencer@gmail.com>
2013-01-09 11:33:05 -0600

    Added header file to import all categories.

---
+++ SmalltalkExtensions/Extensions/SmalltalkExtensions.h
@@ -0,0 +1,15 @@
+//
+//  SmalltalkExtensions.h
+//  SmalltalkExtensions
+//
+//  Created by bracken-dev@GitHub on 1/9/13.
+//
+
+#ifndef SmalltalkExtensions_h
+#define SmalltalkExtensions_h
+
+#import "NSArray+SmalltalkCollections.h"
+#import "NSMutableArray+SmalltalkCollections.h"
+#import "NSString+SmalltalkString.h"
+
+#endif
